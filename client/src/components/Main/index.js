5c1f9f7ecf4d0ad236a536baab1dc311245e3687
tmullen1991 <tmullen1991@gmail.com>
2020-10-29 15:27:58 -0600

    cleaned up

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import API from "../../utils/api";
-import Input from "../Input";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 import "./style.css"
 
 export default function Example() {
@@ -11,11 +12,12 @@ export default function Example() {
   const [num, setNum] = useState(0);
   const [value, setValue] = useState("")
 
+  // API call made to sever to grab random words from DB and store response in words array
   useEffect(() => {
     async function loadWords() {
       const response = await API.getWords();
       const data = await response.data;
-      // suffle from alphabetical
+      // suffle words from alphabetical
       data.sort(() => Math.random() - 0.5);
       setWords(data);
       setNum(0);
@@ -24,7 +26,7 @@ export default function Example() {
     setLoading(false);
   }, []);
 
-  // limit the number of words displayed
+  // display X amount of words at a given time and assign highlight to current word to be typed by user 
   const display = words.slice(num, num + 20).map((x, i) => {
     return (
       <span className={(i === num) ? "current-word" : "inactive"} id={i} key={i}>
@@ -33,7 +35,10 @@ export default function Example() {
     );
   });
 
-  
+  const testInput = (event)=>{
+    setValue(event.target.value)
+    
+  }
   return (
     <div className="container py-3">
       <div className="row py-3 justify-content-center">
@@ -48,7 +53,20 @@ export default function Example() {
         </Card>
       </div>
       <div className="row justify-content-center">
-        <Input />
+      <div className="container py-3">
+        <div className="row px-auto justify-content-center">
+          <InputGroup onChange={testInput} style={{ width: "24rem" }}>
+            <FormControl
+              defaultValue={value}
+              placeholder="Type Words Here :)"
+              aria-label="Type Words Here :)"
+            />
+            <InputGroup.Append>
+              <InputGroup.Text id="timer">00</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+        </div>
+      </div>
       </div>
     </div>
   );
