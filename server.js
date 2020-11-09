81f27405b8a4fcf55737591eaa6f2d1732b08c0d
tmullen1991 <tmullen1991@gmail.com>
2020-11-09 09:38:01 -0700

    fixing routes for heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+require("dotenv").config();
 
 const app = express();
 var PORT = process.env.PORT || 8080;
-
 
 // Serve static content for the app from the "public" directory in the application directory.
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("client/build"));
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI  || "mongodb://localhost/wordDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wordDB", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI  || "mongodb://localhost/wordDB", {
 // Routes
 app.use(routes);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
