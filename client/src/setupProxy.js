4dc31709ff88b85308b9847f5ef6532f837bba54
tmullen1991 <tmullen1991@gmail.com>
2020-11-10 14:06:31 -0700

    http-proxy-middleware errors should be resolved for build

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
-module.exports = app => {
-  app.use(createProxyMiddleware("/", { target: "http://localhost:5000" }));
-};
+module.exports = function(app) {
+  app.use(proxy('/', { target: 'http://[::1]:5000/' }))
+}
