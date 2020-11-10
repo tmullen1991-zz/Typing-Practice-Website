d8191267ae04c51958723f12c1ab5dc632e09b85
tmullen1991 <tmullen1991@gmail.com>
2020-11-10 15:05:45 -0700

    should work

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 8080;
+require("dotenv").config();
 
-// Serve static content for the app from the "public" directory in the application directory.
-
-// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("client/build"));
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Routes
 app.use(routes);
 
@@ -21,9 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wordDB", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log(`Server listening on: http://localhost:` + PORT);
