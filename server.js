9937ae5ec77267c55c12f37f6524a0559a544875
tmullen1991 <tmullen1991@gmail.com>
2020-11-20 18:16:01 -0700

    testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  router.use(function(req, res) {
+  app.use(function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
