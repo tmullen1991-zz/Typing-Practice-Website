d85715dce64752c824e257391672a6e296d70ac4
Tom <tom@Toms-MacBook-Pro.local>
2021-06-23 11:51:40 -0600

    updating to revert

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(root));
+  app.use(express.static("client/build"));
 }
 
 // Routes
