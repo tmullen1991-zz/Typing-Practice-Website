0cbde48cb6ed77ee4be23f88f9c52c5b58d62556
tom <tmullen1991@gmail.com>
2021-07-04 11:17:09 -0600

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Easy from "./pages/Easy";
-import Hard from "./pages/Hard";
 import Home from "./pages/Home";
 import NotFound from "./components/NotFound";
 import Navbar from "./components/Navbar";
@@ -16,8 +14,6 @@ function App() {
         <Jumbotron className="container py-5 my-5 justify-content-center" fluid>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/easy" component={Easy} />
-            <Route exact path="/hard" component={Hard} />
             <Route component={NotFound} />
           </Switch>
         </Jumbotron>
